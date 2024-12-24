49d5dcbabede7fbcad093bb653482f08ae1766e5
khsei <khsei@naver.com>
2024-12-25 00:08:59 +0900

    exit를 명령어 입력받을 때 까지 명령어 입력 하기

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -8,12 +8,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 자바 텍스트 게시판 시작 ==");
-        System.out.print("명령) ");
-        String cmd = sc.nextLine();
 
-        System.out.printf("입력받은 명령어 : %s\n", cmd);
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
 
+            if(cmd.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            System.out.printf("입력받은 명령어 : %s\n", cmd);
+        }
 
         System.out.println("== 자바 텍스트 게시판 종료 ==");
+        sc.close();
         }
     }
