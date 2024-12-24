6999f6c3fcdd44774bf7c0feee48238b3fafd7c6
khsei <khsei@naver.com>
2024-12-25 00:02:02 +0900

    스캐너를 이용한 명령어 입력받기

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -1,10 +1,17 @@
 package com.sbs.java.text_board;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("== 자바 텍스트 게시판 시작 ==");
+        System.out.print("명령) ");
+        String cmd = sc.nextLine();
+
+        System.out.printf("입력받은 명령어 : %s\n", cmd);
 
 
         System.out.println("== 자바 텍스트 게시판 종료 ==");
