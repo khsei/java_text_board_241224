17711cd6c18bbadd5979cc243908c35e41f6b46a
khsei <khsei@naver.com>
2024-12-26 14:16:15 +0900

    '='을 기준으로 파라미터를 세밀하게 쪼개기

--- src/main/java/com/sbs/java/text_board/AppTest.java
+++ src/main/java/com/sbs/java/text_board/AppTest.java
@@ -1,7 +1,5 @@
 package com.sbs.java.text_board;
-
 import java.util.Arrays;
-
 public class AppTest {
     public static void main(String[] args) {
         // 파라미터 분석
@@ -10,9 +8,13 @@ public class AppTest {
         String[] queryStringBits = queryString.split("&");
         System.out.println(Arrays.toString(queryStringBits));
 
-        // for(String bit : queryStringBits) System.out.println(bit);
+        for(String bit : queryStringBits) {
+            String[] bitBits = bit.split("=");
+            String paramName = bitBits[0];
+            String paramValue = bitBits[1];
 
-        Arrays.stream(queryStringBits).forEach(System.out::println);
+            System.out.printf("%s : %s\n", paramName, paramValue);
+        }
 
     }
 }
