f075b180e1142ad2b5beef44c9ad120e46764d29
khsei <khsei@naver.com>
2024-12-26 15:36:29 +0900

    List를 도입하여 paramName과 paramValue를 List에 저장

--- src/main/java/com/sbs/java/text_board/AppTest.java
+++ src/main/java/com/sbs/java/text_board/AppTest.java
@@ -1,37 +1,38 @@
 package com.sbs.java.text_board;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 public class AppTest {
     public static void main(String[] args) {
         // 파라미터 분석
-        String queryString = "a=10&b=20&c=30";
+        String queryString = "d=40&a=10&b=20&c=30&f=15";
         // a=1, b=2, c=3
         String[] queryStringBits = queryString.split("&");
         System.out.println(Arrays.toString(queryStringBits));
 
-        int a = 0;
-        int b = 0;
-        int c = 0;
+        List<String> paramNames = new ArrayList<>();
+        List<Integer> paramValues = new ArrayList<>();
 
-        for(String bit : queryStringBits) {
+
+        for (String bit : queryStringBits) {
             String[] bitBits = bit.split("=");
             String paramName = bitBits[0];
             String paramValue = bitBits[1];
 
-            if(paramName.equals("a")) {
-                a = Integer.parseInt(paramValue);
-            }
-            else if(paramName.equals("b")) {
-                b = Integer.parseInt(paramValue);
-            }
-            else {
-                c = Integer.parseInt(paramValue);
-            }
+            paramNames.add(paramName);
+            paramValues.add(Integer.parseInt(paramValue));
         }
 
-        System.out.printf("a : %d\n", a);
-        System.out.printf("a : %d\n", b);
-        System.out.printf("a : %d\n", c);
+            for (int i = 0; i < paramNames.size(); i++) {
+                String paramName = paramNames.get(i);
+                int paramValue = paramValues.get(i);
 
+                System.out.printf("%s : %d\n", paramName, paramValue);
+            }
+
+
+        }
     }
-}
 
