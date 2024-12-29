63695efdfa61053f6fda92eaa09d57295f1fbbf9
khsei <khsei@naver.com>
2024-12-30 00:42:00 +0900

    쿼리스트링을 분석하여 분리한 데이터를 Map에 저장

--- src/main/java/com/sbs/java/text_board/AppTest.java
+++ src/main/java/com/sbs/java/text_board/AppTest.java
@@ -1,34 +1,43 @@
 package com.sbs.java.text_board;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class AppTest {
     public static void main(String[] args) {
         // 파라미터 분석
-        String queryString = "id=20&subject=자바수업&content=내용&writerName=김철수";
+        String queryString = "id=20&subject=자바수업&content=내용&writerName=김철수&hitCount=20";
         String[] queryStringBits = queryString.split("&");
         System.out.println(Arrays.toString(queryStringBits));
 
-        List<String> paramNames = new ArrayList<>();
-        List<String> paramValues = new ArrayList<>();
+//        Map<String, String> params = new HashMap<>();
+        Map<String, String> params = new LinkedHashMap<>();
 
 
         for (String bit : queryStringBits) {
             String[] bitBits = bit.split("=");
-            String paramName = bitBits[0];
-            String paramValue = bitBits[1];
-
-            paramNames.add(paramName);
-            paramValues.add(paramValue);
+            params.put(bitBits[0], bitBits[1]);
         }
 
-        System.out.println(paramNames);
-        System.out.println(paramValues);
+        System.out.println(params);
 
-        int findIndex = paramNames.indexOf("content");
-        System.out.println(paramValues.get(findIndex));
+        System.out.println("== 원하는 것을 하나하나 가져와서 출력 ==");
+        System.out.printf("id : %d\n", Integer.parseInt(params.get("id")));
+        System.out.printf("subject : %s\n", params.get("subject"));
+        System.out.printf("content : %s\n", params.get("content"));
+        System.out.printf("writerName : %s\n", params.get("writerName"));
+        System.out.printf("hitCount : %s\n", params.get("hitCount"));
+
+        System.out.println("== 반복문을 사용하여 순회 출력 v1 ==");
+        for(String paramName : params.keySet()) {
+            String paramValue = params.get(paramName);
+
+            System.out.printf("%s : %s\n", paramName, paramValue);
         }
+
+        System.out.println("== 반복문을 사용하여 순회 출력 v2 ==");
+        params.forEach((key,value) -> System.out.printf("%s : %s\n", key, value));
     }
+}
 
