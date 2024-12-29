6218c2c1925989b6136e2d1cada1e91536858ead
khsei <khsei@naver.com>
2024-12-30 01:03:11 +0900

    Util.getParams 함수 도입

--- src/main/java/com/sbs/java/text_board/AppTest.java
+++ src/main/java/com/sbs/java/text_board/AppTest.java
@@ -1,43 +1,32 @@
 package com.sbs.java.text_board;
 
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class AppTest {
     public static void main(String[] args) {
         // 파라미터 분석
-        String queryString = "id=20&subject=자바수업&content=내용&writerName=김철수&hitCount=20";
-        String[] queryStringBits = queryString.split("&");
-        System.out.println(Arrays.toString(queryStringBits));
+        String queryString1 = "id=20&subject=자바수업&content=내용&writerName=김철수&hitCount=20";
+        Map<String, String> params1 = Util.getParams(queryString1);
+        System.out.println(params1);
 
-//        Map<String, String> params = new HashMap<>();
-        Map<String, String> params = new LinkedHashMap<>();
+        String queryString2 = "id=20&subject=자바&content=내용2&writerName=김수정&hitCount=40";
+        Map<String, String> params2 = Util.getParams(queryString2);
+        System.out.println(params2);
 
-
-        for (String bit : queryStringBits) {
-            String[] bitBits = bit.split("=");
-            params.put(bitBits[0], bitBits[1]);
-        }
-
-        System.out.println(params);
-
-        System.out.println("== 원하는 것을 하나하나 가져와서 출력 ==");
-        System.out.printf("id : %d\n", Integer.parseInt(params.get("id")));
-        System.out.printf("subject : %s\n", params.get("subject"));
-        System.out.printf("content : %s\n", params.get("content"));
-        System.out.printf("writerName : %s\n", params.get("writerName"));
-        System.out.printf("hitCount : %s\n", params.get("hitCount"));
-
-        System.out.println("== 반복문을 사용하여 순회 출력 v1 ==");
-        for(String paramName : params.keySet()) {
-            String paramValue = params.get(paramName);
-
-            System.out.printf("%s : %s\n", paramName, paramValue);
-        }
-
-        System.out.println("== 반복문을 사용하여 순회 출력 v2 ==");
-        params.forEach((key,value) -> System.out.printf("%s : %s\n", key, value));
     }
 }
 
+class Util {
+    static Map<String, String> getParams(String queryStr) {
+        Map<String, String> params = new LinkedHashMap<>();
+        String[] queryStrBits = queryStr.split("&");
+
+        for(String bit : queryStrBits) {
+            String[] bitBits = bit.split("=");
+
+            params.put(bitBits[0], bitBits[1]);
+        }
+        return params;
+    }
+}
